f15e459b41b6abb6e827ada07da8fbe793765ff1
Other Developer <developer@mail.ru>
2021-12-06 19:14:29 +0300

    05.12.2021 Ненавижу отчёты и интерактивный интерфейс

---
+++ pages/scripts/form.js
@@ -0,0 +1,34 @@
+var submit_button = document.querySelector("#submit_button");
+var message_area = document.querySelector("#message_area");
+var warning_window = document.querySelector("#warning_window")
+var warning_button = document.querySelector("#warning_button");
+var warning_message = document.querySelector("#warning_message");
+var display = false;
+
+function sendMessage() {
+    var text = message_area.value;
+    message_area.value = "";
+    if (text.length > 0)
+        warning("Спасибо за пару красивых слов :)");
+    else
+        warning("Вы забыли написать пару красивых слов");
+}
+
+function warning(message) {
+    warning_window.style.display = "flex";
+    warning_message.textContent = message;
+    display = true;
+}
+
+function hideWarning() {
+    warning_window.style.display = "none";
+    display = false;
+}
+
+hideWarning();
+warning_button.addEventListener("click", function (event) {
+    if (display === true)
+        hideWarning();})
+submit_button.addEventListener("click", function (event) {
+    if (display === false)
+        sendMessage();})
